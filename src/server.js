319de53c46afc4488bd1337e63d9246503411706
Rajendra Ku. Tarai <11242899+tarairajendra@users.noreply.github.com>
2024-11-27 08:47:44 +0530

    add basic ui for chat

--- src/server.js
+++ src/server.js
@@ -1,15 +1,22 @@
 import { createServer } from 'node:http'
 import express from 'express'
+import { dirname, join } from 'node:path'
+import { fileURLToPath } from 'node:url'
 
 const HOST = process.env.HOST || '127.0.0.1'
 const PORT = process.env.PORT || 56789
 
+const __dirname = dirname(fileURLToPath(import.meta.url)) 
 
 const app = express()
 const server = createServer(app)
 
 app.get('/health', (req, res) => {
   res.json({msg: "Healthy"})
+})
+
+app.get('/', (req, res) => {
+  res.sendFile(join(__dirname, 'public/index.html'))
 })
 
 app.listen(PORT, HOST, () => {
