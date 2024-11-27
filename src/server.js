488f5c1d7f48814e3f22ddcfaaea253025267e8c
Rajendra Ku. Tarai <11242899+tarairajendra@users.noreply.github.com>
2024-11-27 09:09:00 +0530

    add socket.io

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ import { createServer } from 'node:http'
 import express from 'express'
 import { dirname, join } from 'node:path'
 import { fileURLToPath } from 'node:url'
+import { Server } from 'socket.io'
 
 const HOST = process.env.HOST || '127.0.0.1'
 const PORT = process.env.PORT || 56789
@@ -10,6 +11,7 @@ const __dirname = dirname(fileURLToPath(import.meta.url))
 
 const app = express()
 const server = createServer(app)
+const io = new Server(server);
 
 app.get('/health', (req, res) => {
   res.json({msg: "Healthy"})
@@ -19,6 +21,16 @@ app.get('/', (req, res) => {
   res.sendFile(join(__dirname, 'public/index.html'))
 })
 
-app.listen(PORT, HOST, () => {
+io.on('connection', sock => {
+  console.log(`connected socket id: ${sock.id}`)
+
+
+
+  sock.on('disconnect', () => {
+    console.log(`socket disconnected: ${sock.id}`)
+  })
+})
+
+server.listen(PORT, HOST, () => {
   console.log(`Server is up and running on ${HOST}:${PORT}`)
 })
